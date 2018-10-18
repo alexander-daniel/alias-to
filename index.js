0b86e6288d1aa1dbefe294864a9d34c76f64ec8c
alexander-daniel <alex.vados@gmail.com>
2018-10-18 17:53:25 -0400

    single script

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const { send } = require('micro');
 const redirect = require('micro-redirect');
 const { parse, format } = require('url');
 const { router, get } = require('microrouter');
-const ShortURL = require('./key-value');
+const kvs = require('keyvalue-xyz');
+const { promisify } = require('util');
+const createToken = promisify(kvs.createToken);
+const getValueForKey = promisify(kvs.getValueForKey);
+const setValueForKey = promisify(kvs.setValueForKey);
 
-const shortURL = new ShortURL('alias-to');
+const KEY = 'alias-to';
+
+const encode = async (url) => {
+  const token = await createToken(KEY);
+  await setValueForKey(token, KEY, url);
+  return token;
+}
+
+const decode = async (token) => await getValueForKey(token, KEY);
 
 module.exports = router(
   get('/create', async (req, res) => {
-    console.error(req.query.url);
     const url = parse(req.query.url);
-    console.error(url);
     url.slashes = true;
 
     if (!Boolean(url.protocol)) {
@@ -24,10 +34,7 @@ module.exports = router(
 
     // Create a token with keyvalue-xyz service, get back a unique `token`.
     // We will store the full url using this token.
-    const token = await shortURL.createToken();
-    await shortURL.setValue(token, formattedURL);
-
-    console.error('created', token, formattedURL);
+    const token = await encode(formattedURL);
 
     send(res, 200, { url: `https://${process.env.DOMAIN_NAME}/${token}` });
   }),
@@ -36,15 +43,13 @@ module.exports = router(
     // User has requested a full URL using a short URL.
     // Ask keyvalue-xyz service for the url we have stored with that token.
     const token = req.params.token;
-    const url = await shortURL.getValue(token);
-
-    console.error('redirect', url);
+    const url = await decode(token);
 
     // Redirect them!
     redirect(res, 302, url);
   }),
 
-  get('/', async (req, res) => {
+  get('/', async (_, res) => {
     const html = fs.readFileSync(`${__dirname}/index.html`, 'utf8');
     send(res, 200, html);
   })
