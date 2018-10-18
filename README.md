e0a0428313c993fe4839b1211c55d187b933ed14
alexander-daniel <alex.vados@gmail.com>
2018-10-18 18:06:38 -0400

    clean it up, add a readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# dead simple url shortener
+- uses keyvalue.xyz so you don't need a database (just keep in mind there is also no privacy)
+- configured for easy deployment on now.sh (just change the subdomain in `package.json` to your own)
+
+## usage
+local
+```bash
+yarn start-dev
+```
--- index.js
+++ index.js
@@ -8,49 +8,28 @@ const { promisify } = require('util');
 const createToken = promisify(kvs.createToken);
 const getValueForKey = promisify(kvs.getValueForKey);
 const setValueForKey = promisify(kvs.setValueForKey);
-
 const KEY = 'alias-to';
 
-const encode = async (url) => {
-  const token = await createToken(KEY);
-  await setValueForKey(token, KEY, url);
-  return token;
-}
-
-const decode = async (token) => await getValueForKey(token, KEY);
-
 module.exports = router(
-  get('/create', async (req, res) => {
+  get('/shorten', async (req, res) => {
     const url = parse(req.query.url);
     url.slashes = true;
+    if (!Boolean(url.protocol)) url.protocol = 'https';
 
-    if (!Boolean(url.protocol)) {
-      console.error('no protocol, adding https');
-      url.protocol = 'https';
-    }
+    // Create a token with keyvalue-xyz service
+    const token = await createToken(KEY);
 
-    // Format the url the user has passed in
-    const formattedURL = format(url);
-
-    // Create a token with keyvalue-xyz service, get back a unique `token`.
-    // We will store the full url using this token.
-    const token = await encode(formattedURL);
+    // We will store the full url using the token.
+    await setValueForKey(token, KEY, format(url));
 
     send(res, 200, { url: `https://${process.env.DOMAIN_NAME}/${token}` });
   }),
 
-  get('/:token', async (req, res) => {
-    // User has requested a full URL using a short URL.
-    // Ask keyvalue-xyz service for the url we have stored with that token.
-    const token = req.params.token;
-    const url = await decode(token);
+  get('/:token', async (req, res) =>
+    redirect(res, 302, (await getValueForKey(req.params.token, KEY)))
+  ),
 
-    // Redirect them!
-    redirect(res, 302, url);
-  }),
-
-  get('/', async (_, res) => {
-    const html = fs.readFileSync(`${__dirname}/index.html`, 'utf8');
-    send(res, 200, html);
-  })
+  get('/', async (_, res) =>
+    send(res, 200, fs.readFileSync(`${__dirname}/index.html`, 'utf8'))
+  )
 )
